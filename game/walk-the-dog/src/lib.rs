dff0b95587886dc1c8cafe15225be4c18e55dd08
lp-peg <tomohiro.katsukura@gmail.com>
2023-10-11 01:43:30 +0900

    with random color

--- game/walk-the-dog/src/lib.rs
+++ game/walk-the-dog/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::thread_rng;
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
@@ -27,12 +29,17 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
     let starting_point = [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)];
-    sierpinski(&context, starting_point, 8);
+    sierpinski(&context, starting_point, (255, 255, 255), 6);
     Ok(())
 }
 
-fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
     let [top, left, right] = points;
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
     context.move_to(top.0, top.1);
     context.begin_path();
     context.line_to(left.0, left.1);
@@ -40,19 +47,31 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64
     context.line_to(top.0, top.1);
     context.close_path();
     context.stroke();
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+    context.fill();
 }
 
-fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], depth: i32) {
+fn sierpinski(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: i32,
+) {
     if depth == 0 {
         return;
     }
     let depth = depth - 1;
     let [top, left, right] = points;
-    draw_triangle(context, [top, left, right]);
+    draw_triangle(context, [top, left, right], color);
     let a = ((top.0 + left.0) / 2.0, (top.1 + left.1) / 2.0);
     let b = ((right.0 + top.0) / 2.0, (top.1 + right.1) / 2.0);
     let c = ((right.0 + left.0) / 2.0, (right.1 + left.1) / 2.0);
-    sierpinski(context, [top, a, b], depth);
-    sierpinski(context, [a, left, c], depth);
-    sierpinski(context, [b, c, right], depth);
+    let next_color = (
+        thread_rng().gen_range(0..255),
+        thread_rng().gen_range(0..255),
+        thread_rng().gen_range(0..255),
+    );
+    sierpinski(context, [top, a, b], next_color, depth);
+    sierpinski(context, [a, left, c], next_color, depth);
+    sierpinski(context, [b, c, right], next_color, depth);
 }
